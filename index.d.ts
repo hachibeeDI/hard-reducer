d8912f9280838219286c96ec788d90f961e330e0
OguraDaiki <8hachibee125@gmail.com>
2019-03-13 16:10:25 +0900

    type improvement

--- index.d.ts
+++ index.d.ts
@@ -7,57 +7,71 @@ export type Action<Payload> = {
 
 // Action Creator
 
-export type ActionCreator<Input, Payload = Input> = {
-  (input: Input): Action<Payload>;
+// In some case i.e. type inference drops the type like `undefined | void | null | never` into `{}`
+type InvalidArg = keyof {};
+
+type ActionWorkerArgument<T = undefined> = T extends undefined
+  ? []
+  : keyof T extends InvalidArg
+  ? []
+  : [T];
+
+export type Callable<Ret, Input = undefined> = {(...arg: ActionWorkerArgument<Input>): Ret};
+
+export type ActionCreator<Payload = void, Input = undefined> = {
   type: string;
-};
+} & Callable<Action<Payload>, Input>;
 
-type AsyncCallable<Input, Payload> = Input extends void
-  ? () => Promise<Payload>
-  : (input: Input) => Promise<Payload>;
 
-export type AsyncActionCreator<Input, Payload> = {
-  started: ActionCreator<Input, void>;
-  resolved: ActionCreator<Input, Payload>;
-  rejected: ActionCreator<Input, Error>;
-} & AsyncCallable<Input, Payload>;
+export type AsyncActionCreator<Payload, Input = undefined> = {
+  started: ActionCreator<void, Input>;
+  resolved: ActionCreator<Payload, Input>;
+  rejected: ActionCreator<Error, Input>;
+} & Callable<Promise<Payload>, Input>;
 
-export type ThunkActionCreator<Input, A = any, R = any> = {
-  started: ActionCreator<Input, void>;
-  resolved: ActionCreator<Input, R>;
-  rejected: ActionCreator<Input, Error>;
-} & AsyncCallable<Input, void>;
+export type ThunkActionCreator<Payload, Input> = {
+  started: ActionCreator<void, Input>;
+  resolved: ActionCreator<Payload, Input>;
+  rejected: ActionCreator<Error, Input>;
+} & Callable<Promise<Payload>, Input>;
+
 
 // Reducer helper
 export type Reducer<State> = {
   (state: State, action: any): State;
   get: () => Reducer<State>;
+
+  case(actionFunc: string, reducer: (state: State, payload: any) => State): Reducer<State>;
   case<Input, Payload>(
-    actionFunc: ActionCreator<Input, Payload>,
+    actionFunc: ActionCreator<Payload, Input>,
     reducer: (state: State, payload: Payload) => State
   ): Reducer<State>;
+
   else(fn: (s: State, a: Action<any>) => State): Reducer<State>;
 };
 
 // API
 
-export const buildActionCreator: (
-  opt?: { prefix?: string }
-) => {
-  createAction<Input, Payload>(
-    t?: string | void,
-    fn?: (input: Input) => Payload
-  ): ActionCreator<Input, Payload>;
+export const buildActionCreator: (opt?: {
+  prefix?: string;
+}) => {
+  createAction<Payload = void, Input = Payload>(t?: string | void): ActionCreator<Payload, Input>;
+  createAction<Payload = void>(t: string, fn: () => Payload): ActionCreator<Payload>;
+  createAction<Payload, Input = Payload>(t: string, fn: (input: Input) => Payload): ActionCreator<Payload, Input>;
 
-  createAsyncAction<Input, Payload>(
+  createAsyncAction<Payload>(
+    t: string | void,
+    fn: () => Promise<Payload>
+  ): AsyncActionCreator<Payload>;
+  createAsyncAction<Payload, Input>(
     t: string | void,
     fn: (input: Input) => Promise<Payload>
-  ): AsyncActionCreator<Input, Payload>;
+  ): AsyncActionCreator<Payload, Input>;
 
-  createThunkAction<Input, A = any, S = any, R = any>(
+  createThunkAction<S, Input, A = any, R = any>(
     t: string | void,
     fn: (input: Input, dispatch: (a: A) => any, getState: () => S) => Promise<R>
-  ): ThunkActionCreator<Input, A, R>;
+  ): ThunkActionCreator<R, Input>;
 };
 
 export const createReducer: <T>(t: T) => Reducer<T>;
